3bc0ed49ad720836fa5b3cb660227f8a67c1c3e6
codahale <codahale@b1e0f299-433e-4bb3-9895-84128a6cfb6a>
2007-02-28 01:58:54 +0000

    Added stable-1.0 branch, just in case.
    git-svn-id: http://bcrypt-ruby.rubyforge.org/svn/branches/stable-1.0@7 b1e0f299-433e-4bb3-9895-84128a6cfb6a

--- ext/bcrypt_ext.c
+++ ext/bcrypt_ext.c
@@ -1,18 +1,21 @@
 #include "ruby.h"
 
+char   *bcrypt_gensalt(u_int8_t, u_int8_t *);
+char   *bcrypt(const char *, const char *);
+
 VALUE mBCrypt;
 VALUE mBCryptInternals;
 
 /* Given a logarithmic cost parameter, generates a salt for use with +bc_crypt+.
  */
 static VALUE bc_salt(VALUE self, VALUE cost, VALUE seed) {
-	return rb_str_new2((char *)bcrypt_gensalt(NUM2INT(cost), RSTRING(seed)->ptr));
+	return rb_str_new2((char *)bcrypt_gensalt(NUM2INT(cost), (u_int8_t *)RSTRING(seed)->ptr));
 }
 
 /* Given a secret and a salt, generates a salted hash (which you can then store safely).
  */
 static VALUE bc_crypt(VALUE self, VALUE key, VALUE salt) {
-	return rb_str_new2((char *)bcrypt(RSTRING(key)->ptr, RSTRING(salt)->ptr));
+	return rb_str_new2((char *)bcrypt(RSTRING(key)->ptr, (char *)RSTRING(salt)->ptr));
 }
 
 /* Create the BCrypt and BCrypt::Internals modules, and populate them with methods. */
